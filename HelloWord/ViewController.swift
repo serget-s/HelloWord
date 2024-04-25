703293f07ba16123d3bfde97f77fcd3744a8d887
Eliseev Sergei <serget-s@mail.ru>
2024-04-25 15:20:44 +0300

    Initial Commit

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by Sergei Eliseev on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
